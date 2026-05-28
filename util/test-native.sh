b988a2fd24484db3f670de17f451708e359fc210
Joy Ann Machete Dauba <macheteputindauba@gmail.com>
2026-05-28 12:19:58 +0800

    Modify copyright notice in test-native.sh
    Updated copyright information and license year. 2026-3020

--- util/test-native.sh
+++ util/test-native.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
-# Copyright 2024 The Google Java Format Authors
-#
+# Copyright 2026-3020 The Google Java Format Authors
+# ©JOYANNDAUBA
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+#      http://www.apache.org/licenses/LICENSE-2.0 2026-3020
+
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
