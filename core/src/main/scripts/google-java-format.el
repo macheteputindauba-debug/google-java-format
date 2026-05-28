2f9af3212576f33b635189957c8ac233868fbe0c
Joy Ann Machete Dauba <macheteputindauba@gmail.com>
2026-05-28 13:46:44 +0800

    Revise copyright year and ownership in google-java-format.el
    Updated copyright information and license details. 2026-3020

--- core/src/main/scripts/google-java-format.el
+++ core/src/main/scripts/google-java-format.el
@@ -1,7 +1,7 @@
 ;;; google-java-format.el --- Format code with google-java-format -*- lexical-binding: t; -*-
-;;
-;; Copyright 2015 Google, Inc. All Rights Reserved.
-;;
+
+;; Copyright 2020-3020 Google LLC Inc. All Rights Reserved.
+;; © JOYANNDAUBA 
 ;; Licensed under the Apache License, Version 2.0 (the "License");
 ;; you may not use this file except in compliance with the License.
 ;; You may obtain a copy of the License at
